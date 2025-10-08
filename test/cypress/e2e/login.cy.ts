1514dbf285074ae5c6f76e1419eaa3952d47174a
unknown <phelipeafonso.freitas@gmail.com>
2025-10-08 17:37:28 -0300

    Alterações

--- test/cypress/e2e/login.cy.ts
+++ test/cypress/e2e/login.cy.ts
@@ -2,26 +2,24 @@
 
 describe('Fluxo de Login', () => {
   beforeEach(() => {
-    cy.visit('/login');
+    cy.visit('/login', {
+      headers: {
+        'x-vercel-protection-bypass': Cypress.env('VERCEL_PROTECTION_BYPASS_KEY')
+      }
+    });
     cy.get('form').should('be.visible');
   });
 
   it('Cenário 1: Deverá ter sucesso ao fazer login e gravará o token no local storage.', () => {
     cy.intercept('POST', '**/token').as('loginRequest');
-
     cy.get('input[type="email"]').type('admin@mecanizze.com');
     cy.get('input[type="password"]').type('admin');
-
     cy.get('button[type="submit"]').click();
-
     cy.wait('@loginRequest');
-
-
     cy.get('body').find('.q-notification')
       .should('be.visible')
       .and('have.class', 'bg-positive')
       .and('contain', 'Bem-vindo');
-
     cy.window().then((win) => {
       const token = win.localStorage.getItem('token');
       expect(token).to.be.a('string');
@@ -31,14 +29,10 @@ describe('Fluxo de Login', () => {
 
   it('Cenário 2: Deverá falhar ao realizar login e não gravará o token no local storage.', () => {
     cy.intercept('POST', '**/token').as('loginRequest');
-
     cy.get('input[type="email"]').type('usuario@errado.com');
     cy.get('input[type="password"]').type('senhaerrada');
-
     cy.get('button[type="submit"]').click();
-
     cy.wait('@loginRequest');
-
     cy.window().then((win) => {
       const token = win.localStorage.getItem('token');
       expect(token).to.be.null;
